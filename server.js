8e813d1df5ca00638e1ff618fa8f36245e10ea92
7twik <99895046+7twik@users.noreply.github.com>
2022-11-12 13:41:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(function (req, res, next) {
 
 // app.get('/user/:id', function (req, res, next) {
 //   res.json({user: 'CORS enabled'})
-// })
+//
 app.use("/auth", authRoute);
 app.use("",routes);
 
